a0ba8ba109bd8baa7ef3f9ba8d3f9da593209321
Don Jose <donkochangal@gmail.com>
2020-04-10 10:38:58 +0530

    Implemented arithmetic operations with functions

---
+++ 00002_arithmeticOperations.js
@@ -0,0 +1,60 @@
+function ArithmeticAdd(num1, num2)
+{
+    var sum = 10 + 20;
+    console.log("sum is " + sum);
+}
+ArithmeticAdd();
+function ArithmeticSub(num3, num4)
+{
+    var difference = 20 - 10;
+    console.log("difference is " + difference);
+}
+ArithmeticSub();
+function ArithmeticProduct(num5, num6)
+{
+    var product = 10 * 10;
+    console.log("product is " + product);
+}
+ArithmeticProduct();
+function ArithmeticQuotient(num7, num8)
+{
+    var quotient = 20 / 10;
+    console.log("quotient is " + quotient);
+}
+ArithmeticQuotient();
+function ArithmeticModulus(num9, num10)
+{
+    var modulus = 45 % 23;
+    console.log("modulus is " + modulus);
+}
+ArithmeticModulus();
+function ArithmeticLess(num11, num12)
+{
+    var num11 = 10;
+    var num12 = 8;
+    if (num12 < num11)
+    {
+        console.log("Num12 is less than num11");
+    }
+}
+ArithmeticLess();
+function ArithmeticGreater(num13, num14)
+{
+    var num13 = 35;
+    var num14 = 40;
+    if (num14 > num13)
+    {
+        console.log("num14 is greater than num13")
+    }
+}
+ArithmeticGreater();
+function ArithmeticEqual(num15, num16)
+{
+    var num15 = 10;
+    var num16 = 10;
+    if (num15 == num16)
+    {
+        console.log("Numbers are equal");
+    }
+}
+ArithmeticEqual();
