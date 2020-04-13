19542ae9f1e78e7c5b287ab1d4d21924275baf43
Don Jose <donkochangal@gmail.com>
2020-04-13 07:52:40 +0530

    converted program to ES6 style

--- 000013_numberOdd.js
+++ 000013_numberOdd.js
@@ -1,5 +1,6 @@
-function numberOdd(number)
-{
+const readlinesync = require('readline-sync')
+const number = readlinesync.question("Enter a number: ");
+const numberOdd = (number) => {
     if ( number % 2 != 0 ) //Checking odd or even
     {
         console.log("the number is odd")
@@ -9,4 +10,4 @@ function numberOdd(number)
         console.log("number is even");
     }  
 }
-numberOdd(78);
+numberOdd(number);
