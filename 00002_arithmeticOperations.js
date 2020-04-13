f7fdadeb03c633d8ce1845e2f6a731411d2ddc83
Don Jose <donkochangal@gmail.com>
2020-04-13 07:31:48 +0530

    Converted this to es6

--- 00002_arithmeticOperations.js
+++ 00002_arithmeticOperations.js
@@ -1,60 +1,57 @@
-function ArithmeticAdd(num1, num2)
-{
-    var sum = 10 + 20;
+const readlinesync = require('readline-sync');
+const num1 = parseInt(readlinesync.question("Enter the first number: "));
+const num2 = parseInt(readlinesync.question("Enter the second number: "));
+
+const ArithmeticAdd = (num1, num2)=> { //Addition
+    const sum = num1 + num2;
     console.log("sum is " + sum);
 }
-ArithmeticAdd();
-function ArithmeticSub(num3, num4)
-{
-    var difference = 20 - 10;
+
+const ArithmeticSub = (num1, num2)=> { //subtraction
+    const difference = num1 - num2;
     console.log("difference is " + difference);
 }
-ArithmeticSub();
-function ArithmeticProduct(num5, num6)
-{
-    var product = 10 * 10;
-    console.log("product is " + product);
+
+const ArithmeticProduct = (num1, num2)=> { //multiplication
+    var product = num1 * num2;
+    console.log("product is " + product); 
 }
-ArithmeticProduct();
-function ArithmeticQuotient(num7, num8)
-{
-    var quotient = 20 / 10;
+
+const ArithmeticQuotient = (num1, num2) => { //division
+    var quotient = num1 / num2;
     console.log("quotient is " + quotient);
 }
-ArithmeticQuotient();
-function ArithmeticModulus(num9, num10)
-{
-    var modulus = 45 % 23;
+
+const ArithmeticModulus = (num1, num2) => { //modulus
+    var modulus = num1 % num2;
     console.log("modulus is " + modulus);
 }
-ArithmeticModulus();
-function ArithmeticLess(num11, num12)
-{
-    var num11 = 10;
-    var num12 = 8;
-    if (num12 < num11)
+
+const ArithmeticLess = (num1, num2) => { //checking less number
+    if (num1 < num2)
     {
-        console.log("Num12 is less than num11");
+        console.log("Num1 is less than num12");
     }
 }
-ArithmeticLess();
-function ArithmeticGreater(num13, num14)
-{
-    var num13 = 35;
-    var num14 = 40;
-    if (num14 > num13)
+
+const ArithmeticGreater = (num1, num2) => { //checking greater number
+    if (num1 > num2)
     {
-        console.log("num14 is greater than num13")
+        console.log("num1 is greater than num2");
     }
 }
-ArithmeticGreater();
-function ArithmeticEqual(num15, num16)
-{
-    var num15 = 10;
-    var num16 = 10;
-    if (num15 == num16)
+
+function ArithmeticEqual(num1, num2) { //checking equal or not
+    if (num1 == num2)
     {
         console.log("Numbers are equal");
     }
 }
-ArithmeticEqual();
+ArithmeticAdd(num1, num2);
+ArithmeticSub(num1, num2);
+ArithmeticProduct(num1, num2);
+ArithmeticQuotient(num1, num2);
+ArithmeticModulus(num1, num2);
+ArithmeticLess(num1, num2);
+ArithmeticGreater(num1, num2);
+ArithmeticEqual(num1, num2);
