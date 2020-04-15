82f2973e9dac49828cff9b8bc4d6f04f7f2d2c76
Don Jose <donkochangal@gmail.com>
2020-04-15 12:46:40 +0530

    Just for updating

--- 006_multiplyArray.js
+++ 006_multiplyArray.js
@@ -2,11 +2,11 @@ const input = require('readline-sync');
 let arrayelem = [];
 arrayelem = input.question("Enter the elements: ");
 console.log("The array is: " + arrayelem);
-const multiplyArray = ()=>{
+const squaresArray = ()=>{
     let square = [];
     for (let i = 0; i < arrayelem.length; i++){
         square.push(arrayelem[i] * arrayelem[i]);
     }
     console.log("Squares of the elements: " + square);
 }
-multiplyArray();
+squaresArray();
