07fe5846b85536c51d9e8a038ae8c9b621b7b12b
Don Jose <donkochangal@gmail.com>
2020-04-15 15:13:53 +0530

    remove duplicate file

--- 000012_Multiplicationtable.js
+++
@@ -1,8 +0,0 @@
-const Multplicationtable = ()=> {
-    for (let i = 1; i <= 12; i++){
-        for (let n = 1; n <= 10; n++){
-            console.log(i + " * " + n + " = " + i * n);
-        }
-    }
-}
-Multplicationtable();
