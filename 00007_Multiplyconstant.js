7e83e80dcc11ac30118a7187b9c5f92a80149662
Don Jose <donkochangal@gmail.com>
2020-04-15 14:14:29 +0530

    Mutiple array with a constant

---
+++ 00007_Multiplyconstant.js
@@ -0,0 +1,10 @@
+const readline = require('readline-sync');
+const arrayval = readline.question("Enter the elements: ");
+const arr = arrayval.split(',');
+const val = 10; 
+const multiplyCons = ()=> {
+    for (let i = 0; i < arr.length; i++){
+        console.log(arr[i] * val);
+    }
+}
+multiplyCons();
