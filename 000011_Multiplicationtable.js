25a5b6497a594291843a6136149db774f05f214c
Don Jose <donkochangal@gmail.com>
2020-04-17 16:57:11 +0530

    Alingnment corrected

--- 000011_Multiplicationtable.js
+++ 000011_Multiplicationtable.js
@@ -1,8 +1,8 @@
-const Multplicationtable = ()=> {
+const multplicationTable = () => {
     for (let i = 1; i <= 12; i++){
         for (let n = 1; n <= 10; n++){
             console.log(i + " * " + n + " = " + i * n);
         }
     }
 }
-Multplicationtable();
+multplicationTable();
