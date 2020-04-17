4c4792f549e714d6e7704e1e91086a4f8c7df149
Don Jose <donkochangal@gmail.com>
2020-04-17 14:14:36 +0530

    Deleted duplicate and corrected the PR

--- 000019_VowelConsonant.js
+++ 000019_VowelConsonant.js
@@ -1,12 +1,13 @@
 let readline = require('readline-sync');
-let VowelsConsonant = (chara)=>{
-    if(chara == 'a'||chara == 'A' || chara == 'e' || chara == 'E'|| chara == 'i'|| chara == 'I'
-    || chara == 'o' || chara == 'O' || chara =='u'|| chara == 'U') {
+let checkchar = readline.question("Enter a character: ");
+
+const vowelsConsonant = (chara) => {
+    if(chara === 'a' || chara === 'A' || chara === 'e' || chara === 'E' || chara === 'i' || chara == 'I'
+    || chara === 'o' || chara === 'O' || chara ==='u' || chara === 'U') {
         console.log(chara + " " + "is a Vowel");
         }
         else{
             console.log(chara + " " + "is a Consonant");
         }
 }
-let checkchar = readline.question("Enter a character: ");
-VowelsConsonant(checkchar);
+vowelsConsonant(checkchar);
