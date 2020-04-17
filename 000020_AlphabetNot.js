dbd98446d226a1794c22f50b619a71b2b66437d2
Don Jose <donkochangal@gmail.com>
2020-04-17 12:26:27 +0530

    Corrected alignment

--- 000020_AlphabetNot.js
+++ 000020_AlphabetNot.js
@@ -1,19 +1,14 @@
 const readline = require('readline-sync');
-const alphabetNot = (alphabet)=> 
-{
-    if (( alphabet >= 'a' && alphabet <= 'z') || (alphabet >= 'A' && alphabet <= 'Z'))
-    {
+const alphabetNot = (alphabet) => {
+    if (( alphabet >= 'a' && alphabet <= 'z') || (alphabet >= 'A' && alphabet <= 'Z')){
         console.log("It's an alphabet");
     }
-    else if (alphabet >= '!' && alphabet <= '*')
-    {
+    else if (alphabet >= '!' && alphabet <= '*'){
         console.log("It's a special character");
     }
-    else
-    {
+    else{
         console.log("It's a number");
     }
 }
 const readval = readline.question("Enter the character: ");
-//console.log(alphabet);
 alphabetNot(readval);
