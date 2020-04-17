c74ad9b953f56716d54a2fe46112c07ea393ae1f
Don Jose <donkochangal@gmail.com>
2020-04-17 17:14:14 +0530

    PR updated with proper alignment

--- 00006_SumofArray.js
+++ 00006_SumofArray.js
@@ -2,10 +2,11 @@ const readline = require('readline-sync');
 const array = readline.question("Enter the elements: ");
 const arraysize = array.split(',');
 let sum = 0;
-const sumofArray = () => {
+
+const sumofArray = (array) => {
         for (let i = 0; i < arraysize.length; i++){
             sum = sum + parseInt(arraysize[i]);
         }   
-        console.log("Array sum: " + sum);
+        return sum;
 }
-sumofArray();
+console.log("Array sum:", sumofArray(array));
