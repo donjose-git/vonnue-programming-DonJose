d6f61eae36e7cc5c08b43e5a5fe6c7419b34f471
Don Jose <donkochangal@gmail.com>
2020-04-17 12:03:58 +0530

    Updating program

--- 000013_numberOdd.js
+++ 000013_numberOdd.js
@@ -1,13 +1,11 @@
 const readlinesync = require('readline-sync')
-const number = readlinesync.question("Enter a number: ");
+let number = parseInt(readlinesync.question("Enter a number: "));
 const numberOdd = (number) => {
-    if ( number % 2 != 0 ) //Checking odd or even
-    {
-        console.log("the number is odd")
+    if (number % 2 === 0 ) { //Checking odd or even
+        console.log("The number is even")
     }
-    else
-    {
-        console.log("number is even");
+    else{
+        console.log("The number is odd");
     }  
 }
 numberOdd(number);
