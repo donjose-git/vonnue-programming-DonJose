1a9cc69f3cc1b6784b91c60202af5475421b06a9
Don Jose <donkochangal@gmail.com>
2020-04-17 15:15:22 +0530

    Updated PR

--- 00003_stringOperations.js
+++ 00003_stringOperations.js
@@ -1,8 +1,9 @@
 const readline = require('readline-sync');
-const str1 = readline.question("Enter string1: ");
-const str2 = readline.question("Enter string2: ");
+let str1 = readline.question("Enter string1: ");
+let str2 = readline.question("Enter string2: ");
+
 const stringConcat = (str1, str2) => { //Concatination
-    console.log("Concatinated string: ", str1 + " " + str2);
+    return str1 + " " + str2;
 }
 
 const stringReverse = (str1) => { //String reverse
@@ -11,23 +12,23 @@ const stringReverse = (str1) => { //String reverse
     {   
         stringrev += str1[i];
     }
-    console.log("Reversed string: " + stringrev);
+    return stringrev;
 }
 
-const reverseconcat = (str1, str2) => { //Reversed concatination
+const reversedConcatination = (str1, str2) => { //Reversed concatination
     const concat = str1 + str2;
     var concatrevstring = "";
     for (var i = concat.length - 1; i >= 0; i--)
     {
         concatrevstring += concat[i];
     }
-    console.log("Reversed Concatination:" + concatrevstring);
+    return concatrevstring;
 }
 const splitString = (str1) => { //String splitting
     const result = str1.split("");
-    console.log("Splitted string: " + result);
+    return result;
 }
-stringConcat(str1, str2);
-stringReverse(str1);
-reverseconcat(str1, str2);
-splitString(str1);
+console.log("Concatinated string:", stringConcat(str1, str2));
+console.log("Reversed string: ", stringReverse(str1));
+console.log("Reversed concatination:", reversedConcatination(str1, str2));
+console.log("Splitted string:", splitString(str1));
