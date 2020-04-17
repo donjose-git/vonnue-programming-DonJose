516e44d895f5daaf179fc7fb9b57b729554eba82
Don Jose <donkochangal@gmail.com>
2020-04-17 13:34:38 +0530

    Seperated to function and corrected alignment

--- 000022_Uppserlowercase.js
+++ 000022_Uppserlowercase.js
@@ -1,14 +1,22 @@
 const readline = require('readline-sync');
-const UppercaseLower = (character)=> {
+const upperCase = (character) => {
     if ( character >= 'A' && character <= 'Z') { //Checking Uppercase
         console.log("The character is a  Uppercase")
     }
-        if (character >= 'a' && character <= 'z'){ //Checking Lowercase
+}
+
+const lowerCase = (character) => {
+    if (character >= 'a' && character <= 'z'){ //Checking Lowercase
         console.log("The character is lower case")
     }
-   else if(character >= '!' && character <= '*'){ //Checking Special character
+}
+
+const specialChar = (character) => {
+    if(character >= '!' && character <= '*'){ //Checking Special character
         console.log("It's a special character")
     }
 }
 const character = readline.question("Enter the character: ");
-UppercaseLower(character);
+upperCase(character);
+lowerCase(character);
+specialChar(character);
