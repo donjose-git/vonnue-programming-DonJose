f2e10eb529ebbb4afbf15024a3e32fe1b974502e
Don Jose <donkochangal@gmail.com>
2020-04-17 14:46:12 +0530

    Alignment corrected

--- 000037_Lepyear.js
+++ 000037_Lepyear.js
@@ -1,11 +1,11 @@
 let input = require('readline-sync');
-const Leapyear = (year) =>{
-    if (year % 4 == 0){
+const leapyear = input.question("Enter a year: ");
+const leapYear = (year) => {
+    if ((year % 4 === 0) && (year % 100 != 0) || (year % 400 === 0)){
         console.log("It's leap year");
     }
     else{
         console.log("Not a leap year");
     }
 }
-const leapyear = input.question("Enter a year: ");
-Leapyear(leapyear);
+leapYear(leapyear);
