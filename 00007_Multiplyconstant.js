e8c956fa42816c89ec5c8c9af5c2d3b29191c3b5
Don Jose <donkochangal@gmail.com>
2020-04-17 17:08:39 +0530

    Corrected Alignment

--- 00007_Multiplyconstant.js
+++ 00007_Multiplyconstant.js
@@ -2,7 +2,8 @@ const readline = require('readline-sync');
 const arrayval = readline.question("Enter the elements: ");
 const arr = arrayval.split(',');
 const val = 10; 
-const multiplyCons = ()=> {
+
+const multiplyCons = () => {
     for (let i = 0; i < arr.length; i++){
         console.log(arr[i] * val);
     }
