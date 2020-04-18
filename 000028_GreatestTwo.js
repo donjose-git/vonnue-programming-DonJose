b320c0190bc4c376ea32bce6355fa3f1471201a7
Don Jose <donkochangal@gmail.com>
2020-04-18 16:55:49 +0530

    Checked greatest of two

---
+++ 000028_GreatestTwo.js
@@ -0,0 +1,9 @@
+const greatestTwo = (number1, number2) => {
+    if (number1 > number2) {
+        return "number1 is greater than number2";
+    }
+    else {
+        return "number1 is less than number2";
+    }
+}
+console.log(greatestTwo(22, 8));
