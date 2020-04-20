131aae47d572a447c94c5f584da212640533a0b8
Don Jose <donkochangal@gmail.com>
2020-04-20 10:31:54 +0530

    Implement a program to check the ASCII value from a character

---
+++ 000021_ASCIIValues.js
@@ -0,0 +1,7 @@
+const readline = require('readline-sync');
+const character = readline.question("Enter a character: ");
+
+const asciiValues = (character) => {
+    console.log(character.charCodeAt(0));
+}
+asciiValues(character);
