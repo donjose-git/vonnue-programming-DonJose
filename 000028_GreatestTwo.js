5adfbebc152c9fe7a0fccc5e85dce1cfdb41bd15
Don Jose <donkochangal@gmail.com>
2020-04-20 21:40:33 +0530

    Program to check greatest of two

--- 000028_GreatestTwo.js
+++ 000028_GreatestTwo.js
@@ -1,12 +1,12 @@
 const greatestTwo = (number1, number2) => {
+
     if (number1 > number2) {
         return "number1 is greater";
-    }
-    else if (number2 > number1) {
+    }else if (number2 > number1) {
         return "number2 is greater";
-    }
-    else {
+    }else {
         return "Both are equal"
     }
 }
+
 console.log(greatestTwo(10, 15));
