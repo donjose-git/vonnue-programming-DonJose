05297e336b90c5d8374f4f504ed07f9c4feff635
Don Jose <donkochangal@gmail.com>
2020-04-20 21:24:53 +0530

    Program to print next 20 leap years

--- 000018_Leapyearnext20.js
+++ 000018_Leapyearnext20.js
@@ -1,13 +1,12 @@
 const input = require('readline-sync');
-const leapyear20 = (year)=> {
+const leapyear20 = (year) => {
     let i = 20;
     while (i >= 0){
-        if (year % 4 == 0){
+        if (year % 4 === 0){
             console.log(year);
             year++;
             i--; 
-        }
-        else{
+        }else {
             year++;
         }
     }
