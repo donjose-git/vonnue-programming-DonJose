499b81dba696d71c4cbd6eb70277b4e8a5536dde
Don Jose <donkochangal@gmail.com>
2020-04-21 09:38:42 +0530

    Addressed review comment

--- 000013_numberOdd.js
+++ 000013_numberOdd.js
@@ -3,8 +3,7 @@ let number = parseInt(readlinesync.question("Enter a number: "));
 const numberOdd = (number) => {
     if (number % 2 === 0 ) { //Checking odd or even
         console.log("The number is even")
-    }
-    else{
+    }else {
         console.log("The number is odd");
     }  
 }
