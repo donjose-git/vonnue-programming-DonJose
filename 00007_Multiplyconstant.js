f6f243ff27ba7edfa5639815923194c62d5854b5
Don Jose <donkochangal@gmail.com>
2020-04-21 16:07:50 +0530

    Updated PR

--- 00007_Multiplyconstant.js
+++ 00007_Multiplyconstant.js
@@ -1,11 +1,11 @@
 const readline = require('readline-sync');
 const arrayval = readline.question("Enter the elements: ");
 const arr = arrayval.split(',');
-const val = 10; 
 
-const multiplyCons = () => {
+const multiplyArrayConstant = (value) => {
     for (let i = 0; i < arr.length; i++){
-        console.log(arr[i] * val);
+        console.log(arr[i] * value);
     }
 }
-multiplyCons();
+
+multiplyArrayConstant(10);
