8670eab726b2eb7830679618a8323e0516dcd678
Don Jose <donkochangal@gmail.com>
2020-04-23 22:18:10 +0530

    PR Updated

--- 000029_GreatestThree.js
+++ 000029_GreatestThree.js
@@ -1,12 +1,11 @@
 const greatestOfThree = (number1, number2, number3) => {
     if (number1 > number2 && number1 > number3) {
         return "number1 is greater";
-    }
-    else if (number2 > number3) {
+    } else if (number2 > number3) {
         return "number2 is greater";
-    }
-    else {
+    } else {
         return "number3 is greater"
     }
 }
+
 console.log(greatestOfThree(33, 44, 55));
